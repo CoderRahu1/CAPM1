06123f4e638c934f7125208b8f8b8d0ede630fc5
CoderRahu1 <rahulpillai1221@gmail.com>
2025-07-22 16:21:09 +0530

    call back function added

--- 4-callback.js
+++ 4-callback.js
@@ -1,3 +1,22 @@
 setTimeout(()=> {
     console.log('Two seconds are up')
 }, 2000);    // this will wait for 2 seconds then it will be printed.
+
+
+const names = ['Andrew', 'Jen', 'Jess']
+const shortNames = names.filter((name) => {
+    return name.length = 4
+})
+
+// so here whichever in list has length 4 is stored in shortNames
+
+const geocode =(address, callback) => {
+    const data = {
+        latitude: 0,
+        longitude: 0
+    }
+    return data
+}
+
+const data = geocode('Philadelphia')   // this will call geocode function with parameter address
+console.log(data)
