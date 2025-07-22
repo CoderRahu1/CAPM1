eb076ca6d87f38942277c2e0fdfdf5b645ef4a6b
CoderRahu1 <rahulpillai1221@gmail.com>
2025-07-22 18:23:49 +0530

    callbacksum

--- 4-callback.js
+++ 4-callback.js
@@ -11,12 +11,31 @@ const shortNames = names.filter((name) => {
 // so here whichever in list has length 4 is stored in shortNames
 
 const geocode =(address, callback) => {
-    const data = {
+    setTimeout(()=>{
+        const data = {
         latitude: 0,
         longitude: 0
     }
-    return data
+    //return data
+    callback(data)
+    }, 2000)
+
 }
 
-const data = geocode('Philadelphia')   // this will call geocode function with parameter address
-console.log(data)
+//const data = geocode('Philadelphia')   // this will call geocode function with parameter address
+
+// below is the way to call a callback function
+geocode('Philadelphia', (data)=>{
+    console.log(data)
+})
+
+// console.log(data)
+
+
+
+
+////////////////////////////////////////////////////////////
+
+
+
+
