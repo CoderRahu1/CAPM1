6d26d56cfc473804bc9d40070fb8a0fe6f6d2120
CoderRahu1 <rahulpillai1221@gmail.com>
2025-07-23 07:30:53 +0530

    D8 Express js

--- httpres.js
+++ httpres.js
@@ -1,28 +1,28 @@
-const http = require('http')
+const https = require('https')
 
-const url = 'http://api.weatherstack.com/current?access_key=c085b1301fd814a13bc57ab7469ce56b&query=New York'
+const url = 'https://api.weatherstack.com/current?access_key=c085b1301fd814a13bc57ab7469ce56b&query=New York'
 
-
-
-const request = http.request(url, (response)=>{
-
+const request = https.request(url, (response) => {
     let data = ''
-    response.on('data', (chunk)=> {
-        data = data + chunk.toString()
 
-    })
-    response.on('end', (chunk)=> {
-
-        const body = JSON.parse(data)
-        console.log(body)
-
+    response.on('data', (chunk) => {
+        data += chunk.toString()
     })
 
-        response.error('error', (error)=> {
-        console.log('An Error : ', error)
-
+    response.on('end', () => {
+        try {
+            const body = JSON.parse(data)
+            console.log(body)
+        } catch (e) {
+            console.error('Error parsing response:', e)
+        }
     })
-
-
-
 })
+
+// Properly handle request errors
+request.on('error', (error) => {
+    console.log('An error:', error)
+})
+
+// Don’t forget to end the request
+request.end()
